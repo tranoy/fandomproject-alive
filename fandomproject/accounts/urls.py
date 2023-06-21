53ed63199cacf3053bea34a3b3438a54a7c122b7
Junobee25 <wnsdhqo@naver.com>
2023-06-22 03:05:16 +0900

    FIX : 로그인 회원가입 UI수정

--- fandomproject/accounts/urls.py
+++ fandomproject/accounts/urls.py
@@ -4,7 +4,7 @@ from .views import Join,Login,Checkbox
 app_name = 'accounts'
 
 urlpatterns = [
-    path('', Login.as_view()),
+    path('/', Login.as_view()),
     path('/join',Join.as_view()),
     path('/checkbox', Checkbox.as_view()),
 ]
