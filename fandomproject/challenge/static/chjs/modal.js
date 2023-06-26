a46feb32fd95a46b3d599a0726e23777a695c12d
Junobee25 <wnsdhqo@naver.com>
2023-06-26 14:32:12 +0900

    FIX : 챌린지 업로드 모달 수정

--- fandomproject/challenge/static/chjs/modal.js
+++ fandomproject/challenge/static/chjs/modal.js
@@ -2,14 +2,29 @@
 
 // 모달 창 요소 가져오기
 var modal = document.getElementById("upload-modal");
-var submitBtn = document.getElementById("submit-btn")
+var submitBtn = document.getElementById("submit-btn");
 
 
+// 스크롤바 없애기
 
 // 업로드 버튼 클릭 시 모달 창 열기
-var uploadButton = document.getElementById("upload-button");
-uploadButton.addEventListener("click", function() {
-  modal.style.display = "flex";
+// var uploadButton = document.getElementById("upload-button");
+// uploadButton.addEventListener("click", function() {
+//   modal.style.display = "flex";
+//   overflow : this.hidden
+// });
+$("#video_file").on('change',function(){
+  var fileName = $("#video_file").val();
+  $(".upload-name").val(fileName);
+});
+
+$('#upload-button').click(function (){
+  $('.modal-box').css({
+    display : 'flex',
+  });
+  $(document.body).css({
+    overflow : 'hidden',
+  });
 });
 
 // 모달 창 닫기 버튼 클릭 시 모달 창 닫기
@@ -40,7 +55,7 @@ var videoTitle = document.getElementById("video-title");
 
     })
 
-var videoFile = document.getElementById("video-file");
+var videoFile = document.getElementById("video_file");
     videoFile.addEventListener("change",function(){
         if(videoFile.files.length > 0){
             check2 = true
@@ -63,33 +78,13 @@ var videoFile = document.getElementById("video-file");
 
 
 
-
-
-var selectVideo = document.getElementById("video-file");
-selectVideo.addEventListener("change", function(){
-    const file = selectVideo.files[0];
-    const videourl = URL.createObjectURL(file);
-    console.log("heelo")
-    console.log(file,videourl)
-})
+uploadContentForm = document.getElementById("upload-content-form");
 
 // 업로드 폼 제출 시 처리
 var uploadForm = document.getElementById("upload-form");
 uploadForm.addEventListener("submit", function(event) {
   event.preventDefault();
 
-  // 비디오 업로드 처리 로직 추가
-  var videoTitle = document.getElementById("video-title").value;
-  var videoFile = document.getElementById("video-file");
-  
-  console.log("제목:", videoTitle);
-  console.log("파일명:", videoFile);
- 
-  
-  // 업로드 완료 메시지 또는 다른 처리 로직 추가
-  // 여기서는 간단히 콘솔에 업로드 완료 메시지를 출력하는 예시를 보여줍니다.
-  console.log("비디오 업로드가 완료되었습니다.");
+  uploadContentForm.style.display ="block";
 
-  // 모달 창 닫기
-  modal.style.display = "none";
 });
