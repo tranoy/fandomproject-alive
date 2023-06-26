3cc3c8aceea2b97506e083fc301c91b99465b2f0
Junobee25 <wnsdhqo@naver.com>
2023-06-27 02:11:58 +0900

    FIX : password email 유효성체크 수정

--- fandomproject/accounts/static/accjs/join.js
+++ fandomproject/accounts/static/accjs/join.js
@@ -39,37 +39,124 @@ let email = $('#email');
         }
     });
 
-// $('#join_btn').click(function () {
 
-//     let username = $('#id').val();
-//     let nickname = $('#nick').val();
-//     let password = $('#pw1').val();
-//     let password_2 = $('#pw2').val();
-//     let email = $('#email').val();
 
-    
-//     // ajax 통신
-//     $.ajax({
-//         url : "/login/join",
-//         data : {
-//             username : username,
-//             nickname : nickname,
-//             password : password,
-//             password_2 : password_2,
-//             email : email, 
+// $(document).ready(function() {
+//     // 아이디 중복 검사
+//     $('#id').on('blur', function() {
+//         var username = $(this).val();
+      
+//         $.ajax({
+//         url: '/login/join',  // 아이디 중복 검사를 처리하는 URL
+//         method: 'POST',
+//         data: {
+//             username: username
 //         },
-//         method : "POST",
-//         // 회원가입 성공시 로그인 페이지로
-//         success : function (data) {
-//             console.log("성공");
-//             alert("회원가입 성공했습니다. 로그인 해주세요.")
-//             location.replace("/login");
+//         success: function(response) {
+//             if (response.exists) {
+//             // 아이디가 이미 존재하는 경우 오류 메시지 표시
+//             $('#id').next('label').addClass('warning').text('This ID is already taken');
+//             } else {
+//             // 아이디가 존재하지 않는 경우 오류 메시지 제거
+//             $('#id').next('label').removeClass('warning').text('ID');
+//             }
 //         },
-//         error : function (request, status, error){
-//             console.log("에러");
+//     });
+//     });
+// });
+// $(document).ready(function() {
+//     // 아이디 중복 검사
+//     $('#nick').on('blur', function() {
+//       let nickname = $(this).val();
+//       let username = $('#username').val();  // username 필드 값 가져오기
+//       let password1 = $('#pw1').val();  // password1 필드 값 가져오기
+//       let password2 = $('#pw2').val();  // password2 필드 값 가져오기
+//       let email = $('#email').val();  // email 필드 값 가져오기
+  
+//       // Ajax 요청
+//       $.ajax({
+//         url: '/login/join',  // 서버의 URL 주소
+//         method: 'POST',
+//         headers: { 'X-CSRFToken': '{{ csrf_token }}' },
+//         data: { nickname: nickname,
+//             username: username,
+//             password1: password1,
+//             password2: password2,
+//             email: email },  // 전송할 데이터
+  
+//         success: function(response) {
+//           if (response.exists) {
+//             // 아이디가 이미 존재하는 경우 오류 메시지 표시
+//             $('#nick').next('label').addClass('warning').text('This ID is already taken');
+//           } else {
+//             // 아이디가 존재하지 않는 경우 오류 메시지 제거
+//             $('#nick').next('label').removeClass('warning').text('ID');
+//           }
 //         },
-//         complete : function () {
-//             console.log("완료");
-//         },
-//     })
-// })
+  
+//         error: function(request, status, error) {
+//           console.log('An error occurred during username check:', error);
+//         }
+//       });
+//     });
+//   });
+
+
+$(document).ready(function() {
+  // 비밀번호 일치 여부 확인
+  $('#pw2').on('blur', function() {
+    let password1 = $('#pw1').val();
+    let password2 = $(this).val();
+
+    if (password1 !== password2) {
+      // 비밀번호가 일치하지 않는 경우 오류 메시지 표시
+      $('#pw2').next('label').addClass('warning').text('Passwords do not match');
+    } else {
+      // 비밀번호가 일치하는 경우 오류 메시지 제거
+      $('#pw2').next('label').removeClass('warning').text('PASSWORD CHECK');
+    }
+  })
+})
+
+// post 방식 비동기 처리 ==> account views.py class
+$('#join_btn').click(function () {
+
+    let username = $('#id').val();
+    let nickname = $('#nick').val();
+    let password1 = $('#pw1').val();
+    let password2 = $('#pw2').val();
+    let email = $('#email').val();
+    if (username && nickname && password1 && password2 && email) {
+    // ajax 통신
+    $.ajax({
+        url : "/login/join",
+        data : {
+            username : username,
+            nickname : nickname,
+            password1 : password1,
+            password2 : password2,
+            email : email, 
+        },
+        method : "POST",
+        // 회원가입 성공시 로그인 페이지로
+        success : function (data) {
+            if (data.exists) {
+            // 아이디가 이미 존재하는 경우 오류 메시지 표시
+            $('#email').next('label').addClass('warning').text('This EMAIL is already taken');
+            } else {
+            // 아이디가 존재하지 않는 경우 오류 메시지 제거
+            $('#email').next('label').removeClass('warning').text('ID');
+            console.log("성공");
+            alert("회원가입 성공했습니다. 로그인 해주세요.")
+            location.replace("/login");
+            }
+        },
+        error : function (request, status, error){
+            console.log("에러");
+        },
+        complete : function () {
+            console.log("완료");
+        },
+    })
+}
+})
--- fandomproject/accounts/views.py
+++ fandomproject/accounts/views.py
@@ -3,6 +3,8 @@ from rest_framework.views import APIView
 from .models import User
 from django.contrib.auth.hashers import make_password
 from rest_framework.response import Response
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -12,17 +14,27 @@ class Join(APIView):
         return render(request, "accounts/join.html")
     
     def post(self,request):
+        
         nickname = request.data.get('nickname',None)
         username = request.data.get('username',None)
         password1 = request.data.get('password1',None)
-        password2 = request.data.get('password2')
+        password2 = request.data.get('password2',)
         email = request.data.get('email','')
-
+        print("post")
+        exists = User.objects.filter(email=email).exists()
+        print(exists)
+        if exists:
+            print("post호출")
+            data = {'exists':exists}
+            return JsonResponse(data)
+        
         # 비밀번호 일치하는지 확인
         if password1 == password2:
             User.objects.create(nickname=nickname,email=email,username=username,password=make_password(password1))
             # 로그인 정보 session에 저
             return redirect('/login')
+        
+        
         # 닉네임 중복여부 확인 
 
                
