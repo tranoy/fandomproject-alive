48f1caa10cedf16314510c2a56fd4696ea16ac12
Junobee25 <wnsdhqo@naver.com>
2023-06-27 00:03:45 +0900

    FEAT : 랭킹페이지 뷰,url 수정

--- fandomproject/ranking/urls.py
+++ fandomproject/ranking/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-from . import views
+from .views import RankingMain
 
 app_name = 'ranking'
 
 urlpatterns = [
-    path('', views.index, name='index')
+    path('', RankingMain.as_view(), name='index')
 ]
--- fandomproject/ranking/views.py
+++ fandomproject/ranking/views.py
@@ -1,7 +1,24 @@
+from django.db import models
 from django.shortcuts import render
-
+from rest_framework.views import APIView
+from challenge.models import Score, Ref_Video
+from django.db.models import Count
 # Create your views here.
 
 
-def index(request):
-    return render(request, 'ranking/ranking.html')
+
+class RankingMain(APIView):
+    def get(self,request):
+        score_count = Score.objects.count()
+        scores = Score.objects.order_by('-score')
+        
+        result = []
+        for score in scores:
+            try:
+                ref_video = Ref_Video.objects.get(id=score.ref_id)
+                result.append([score.nickname, score.score, ref_video.title, ref_video.singer])
+            except Ref_Video.DoesNotExist:
+                pass
+        context = {'score_count' : score_count,
+                   'result' : result}
+        return render(request, 'ranking/ranking.html', context)
