0d87f6bf1d20ba045ee2a25242eb11e8c91cab17
Junobee25 <wnsdhqo@naver.com>
2023-06-26 23:38:44 +0900

    FIX : admin Video, Ref_Video, ScoreAdmin 수정

--- fandomproject/challenge/admin.py
+++ fandomproject/challenge/admin.py
@@ -1,5 +1,16 @@
 from django.contrib import admin
-from .models import Video, Ref_Video
+from .models import *
 
+class VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'video_file', 'ref_id')
+    
+class Ref_VideoAdmin(admin.ModelAdmin):
+    list_display = ('id', 'title', 'singer', 'video_file')
+    list_filter = ['title', 'singer']
+    
+class ScoreAdmin(admin.ModelAdmin):
+    list_display = ('nickname', 'score', 'video_file', 'ref_id', 'create_at')
+    list_filter = ['nickname', 'score']
+    
 admin.site.register(Video)
 admin.site.register(Ref_Video)
