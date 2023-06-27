766934b7df4acf0f53f4e4cbf851962d85c99601
Junobee25 <wnsdhqo@naver.com>
2023-06-27 16:24:19 +0900

    FIX : 로그인정보있으면 Challenge사용가능 수정

--- fandomproject/challenge/static/chjs/main.js
+++ fandomproject/challenge/static/chjs/main.js
@@ -25,3 +25,22 @@
 
 })(jQuery);
 
+
+  
+
+
+let entranceElements = document.getElementsByClassName('entrance_1');
+
+for (let i = 0; i < entranceElements.length; i++) {
+  entranceElements[i].addEventListener('click', function(event) {
+    console.log("발동1");
+    let sessionData = document.getElementById('check').textContent;
+    console.log(sessionData);
+    if (sessionData == '') {
+      event.preventDefault();
+      console.log("발동2");
+      alert("로그인 후 사용하세요");
+      window.location.href = '/login';
+    }
+  });
+}
