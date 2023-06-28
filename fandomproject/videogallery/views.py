58f3a2c15b614e2c133a4932a940ec51d9c6b3d7
Junobee25 <wnsdhqo@naver.com>
2023-06-28 13:43:04 +0900

    FEAT : 페이지네이션 수정 및 challenge views.py 수정

--- fandomproject/videogallery/views.py
+++ fandomproject/videogallery/views.py
@@ -1,7 +1,11 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
 from .models import Gallery
+from challenge.models import Score, Ref_Video
 from accounts.models import User
+from django.db.models import Q
+from django.http import JsonResponse
+
 
 # Create your views here.
 
@@ -9,18 +13,45 @@ from accounts.models import User
 
 class GalleryMain(APIView):
     def get(self,request):
-        video_list = Gallery.objects.all() # select * from content_video
-        try:
-            # 세션 데이터 가져오기
-            nickname = request.session['nickname']
-            user = User.objects.filter(nickname=nickname).first()
-            print(user)
-        except KeyError:
-            nickname = None
-            user = None
-
-        return render(request, "videogallery/videogallery.html",context=dict(videos=video_list,user=user))
+        query = request.GET.get('query')
+        if query:
+            scores = Score.objects.filter(Q(nickname__icontains=query)).all()
+        else:
+            scores = Score.objects.all()
+            
+        result = []
+        for score in scores:
+            try:
+                # 세션 데이터 가져오기
+                # nickname = request.session['nickname']
+                # user = User.objects.filter(nickname=nickname).first()
+                # #print(user)
+                
+                ref_video = Ref_Video.objects.get(id=score.ref_id)
+                result.append([score.id, score.nickname, score.score, score.text, ref_video.title, ref_video.singer])
+                print(score.id)
+            except Ref_Video.DoesNotExist:
+                pass
         
+        context = {
+            'result' : result,
+            'query' : query
+        }
+        return render(request, "videogallery/videogallery.html", context)
+    def post(self, request):
+        if request.method == 'POST':
+            id = request.POST.get('id')
+            action = request.POST.get('action')
+            print(id, action)
+            if action == 'update':
+                new_text = request.POST.get('text')
+                Score.objects.filter(id=id).update(text=new_text)
+                return JsonResponse({'message': 'Row updated successfully'})
+            elif action == 'delete':
+                Score.objects.filter(id=id).delete()
+                return JsonResponse({'message': 'Row deleted successfully'})
+
+        return JsonResponse({'message': 'Invalid request'})
 
 
 
