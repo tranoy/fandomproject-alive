94f959b0d89df045b8337fedb5b640b1bfdecbd0
Junobee25 <wnsdhqo@naver.com>
2023-06-29 16:09:13 +0900

    FIX : 비디오 상세보기 페이지 이미지 크기 수정

--- fandomproject/videogallery/urls.py
+++ fandomproject/videogallery/urls.py
@@ -2,15 +2,18 @@ from django.urls import path
 from . import views
 from django.conf import settings
 from django.conf.urls.static import static
-from .views import *
+from .views import GalleryMain, GalleryMore
 
 app_name = 'videogallery'
 
 urlpatterns = [
-    path('', GalleryMain, name='index'),
-    path('/<int:pk>',GalleryMore.as_view(),name='more'),
-    path('/edit', Edit, name='edit'),
-    # path('/update', BoardUpdate, name='update'),
+    path('', GalleryMain.as_view(), name='index'),
+    path('/<int:pk>', GalleryMore.as_view(), name='more'),
+    path('/<int:pk>/Edit', views.GoUpdate, name='goto_update'),
+    #path('/<int:pk>/update', views.Edit, name='update'),
+    path('/<int:pk>/update', GalleryMain.as_view(), name='update'),
+    path('/<int:pk>/delete', views.delete, name='delete')
+    #path('videogallery/', views.update_or_delete_row, name='videogallery'),
 ]
 
 if settings.DEBUG:
--- fandomproject/videogallery/views.py
+++ fandomproject/videogallery/views.py
@@ -1,88 +1,88 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from rest_framework.views import APIView
 from .models import Gallery
 from challenge.models import Score, Ref_Video
 from accounts.models import User
 from django.db.models import Q
 from django.http import JsonResponse
-
+from making.models import TransformedLog
+from django.core.exceptions import ObjectDoesNotExist
 
 # Create your views here.
 
 # /videogallery 경로 인식 후 index함수가 호출 됨 index함순s html을 렌더링 context포함해서
 
-def Main(request):
-    query = request.GET.get('query')
-    if query:
-        scores = Score.objects.filter(Q(nickname__icontains=query)).all()
-    else:
-        scores = Score.objects.all()
-        
-    result = []
-    for score in scores:
-        try:
-            # 세션 데이터 가져오기
-            # nickname = request.session['nickname']
-            # user = User.objects.filter(nickname=nickname).first()
-            # #print(user)
+class GalleryMain(APIView):
+    def get(self,request):
+        query = request.GET.get('query')
+        if query:
+            scores = Score.objects.filter(Q(nickname__icontains=query)).all()
+        else:
+            scores = Score.objects.all()
             
-            ref_video = Ref_Video.objects.get(id=score.ref_id)
-            result.append([score.id, score.nickname, score.score, score.text, ref_video.title, ref_video.singer])
-            # print(score.id)
-        except Ref_Video.DoesNotExist:
-            pass
-    result = reversed(result)
-    context = {
-        'result' : result,
-        'query' : query
-    }
-    return context
+        result = []
+        for score in scores:
+            try:
+                mk_image = TransformedLog.objects.filter(nickname=score.nickname).order_by('-date').first()
+            except ObjectDoesNotExist:
+                # Handle the case when TransformedLog object does not exist
+                mk_image = None
+            
+            try:
+                ref_video = Ref_Video.objects.get(id=score.ref_id)
+            except ObjectDoesNotExist:
+                # Handle the case when Ref_Video object does not exist
+                ref_video = None
 
-def GalleryMain(request):
-    context = Main(request)
-    
-    if request.method == 'POST':
-        text = request.POST.get('text')  # 게시글 내용 가져오기
-        video_file = request.POST.get('output_path')
-        print(video_file)
-        print('-' * 300, text)
-        nickname = request.session['nickname']
-        score = Score.objects.filter(nickname=nickname).order_by('-id').first()
-        ref_video = Ref_Video.objects.get(id=score.ref_id)
-        ref_video.title = ref_video.title.replace(' ', '_')
-        # video_file = f"output/{nickname}_{ref_video.title}_output.mp4"  # 동영상 파일 업데이트
-        # video_file = request.FILES.get('video')  # 업로드된 동영상 파일 가져오기
+            result.append([score.id, score.nickname, score.score, score.text, ref_video.title if ref_video else '', ref_video.singer if ref_video else '', mk_image.image_url if mk_image else '/media/20230424_002724.png'])
+            print(score.id)
+        context = {
+            'result' : result,
+            'query' : query
+        }
+        return render(request, "videogallery/videogallery.html", context)
+    def post(self, request):
+        if request.method == 'POST':
+            id = request.POST.get('id')
+            action = request.POST.get('action')
+            print(id, action)
+            if action == 'update':
+                new_text = request.POST.get('text')
+                Score.objects.filter(id=id).update(text=new_text)
+                return JsonResponse({'message': 'Row updated successfully'})
+            elif action == 'delete':
+                Score.objects.filter(id=id).delete()
+                return JsonResponse({'message': 'Row deleted successfully'})
 
-        score = Score.objects.filter(nickname=nickname, ref_id=score.ref_id).order_by('-id').first()
-        score.text = text
-        score.video_file = video_file
-        score.save()
-        
-        context = Main(request)
-    return render(request, "videogallery/videogallery.html", context)
-
-
-
-
-def Edit(request):
-    if request.method == 'POST':
-        id = request.POST.get('id')
-        action = request.POST.get('action')
-        print(id, action)
-        if action == 'update':
-            new_text = request.POST.get('text')
-            Score.objects.filter(id=id).update(text=new_text)
-            return JsonResponse({'message': 'Row updated successfully'})
-        elif action == 'delete':
-            Score.objects.filter(id=id).delete()
-            return JsonResponse({'message': 'Row deleted successfully'})
-
-    return JsonResponse({'message': 'Invalid request'})
-
+        return JsonResponse({'message': 'Invalid request'})
 
 class GalleryMore(APIView):
-    def get(self,request,pk):
-        score = Score.objects.filter(id=pk)
-        context = {'score' : score}
+    def get(self, request, pk):
+        scores = Score.objects.filter(id=pk)
+        result = []
+        for score in scores:
+            try:
+                ref_video = Ref_Video.objects.get(id=score.ref_id)
+                result.append([score.id, score.nickname, score.score, score.text, score.create_at, score.video_file, ref_video.title, ref_video.singer])
+                print(result)
+            except Ref_Video.DoesNotExist:
+                pass
+        context = {'result': result }
         print(score)
-        return render(request, "videogallery/more.html",context)
+        return render(request, "videogallery/more.html", context)
+
+def GoUpdate(request, pk):
+    context = {'pk' : pk}
+    return render(request, 'videogallery/videoupdate.html', context)
+    
+def Edit(request, pk):
+
+    if request.method == 'POST':
+        new_text = request.POST.get('new_text')
+        Score.objects.filter(id=pk).update(text=new_text)
+        return redirect('/videogallery/Edit')
+
+def delete(request, pk):
+    Score.objects.filter(id=pk).delete()
+    
+    return redirect('/videogallery')
