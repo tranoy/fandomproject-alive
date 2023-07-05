1ebeb2305fe6c6a0a79a8bf60447b3173af65591
Junobee25 <wnsdhqo@naver.com>
2023-07-06 02:22:17 +0900

    PERF : 불필요 CSS 제거 렌더링 최적화

--- fandomproject/making/static/mkjs/main.js
+++ fandomproject/making/static/mkjs/main.js
@@ -1,12 +1,3 @@
-/*  ---------------------------------------------------
-  Template Name: DJoz
-  Description:  DJoz Music HTML Template
-  Author: Colorlib
-  Author URI: https://colorlib.com
-  Version: 1.0
-  Created: Colorlib
----------------------------------------------------------  */
-
 'use strict';
 
 (function ($) {
